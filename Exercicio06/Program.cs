b1a5004796450001f1b6802d3968a910b59b7726
Charles Junior <charlesjunnr@gmail.com>
2023-03-30 17:02:12 -0300

    Feitas algumas correções.

--- Exercicio06/Program.cs
+++ Exercicio06/Program.cs
@@ -6,46 +6,54 @@ namespace Exercicio06 //Exercicio do arquivo com as cidades.
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static string[] ObterCidadesEhEstados(string caminhoDoArquivo)
         {
-            //precisamos ler o arquivo primeiramente
-            string arquivo = File.ReadAllText(@"C:\Users\Charles Junior\source\repos\ListaExerciciosString\Exercicio06\Dados\Cidades.csv");
-
-            //precisamos pegar as linhas dos arquivos
-            string[] linhas = arquivo.Split("\r\n");
+            string[] linhas = caminhoDoArquivo.Split("\r\n");
 
             //pegar a cidade e o estado 
             string[] cidadesEhEstados = new string[linhas.Length];
 
-            int posicaoCidade = 2;
-            int posicaoEstado = 3;
+            const int POSICAO_CIDADE = 2; // utilizamos const para não modificar o valor de uma variável
+            const int POSICAO_ESTADO = 3;
             int j = 0;
             for (int i = 1; i < linhas.Length; i++)
             {
                 string[] colunas = linhas[i].Split(";");
-                cidadesEhEstados[j] = colunas[posicaoCidade] + ";" + colunas[posicaoEstado];
+                cidadesEhEstados[j] = colunas[POSICAO_CIDADE] + ";" + colunas[POSICAO_ESTADO];
                 j++;
             }
+            return cidadesEhEstados;
+        }
+        static void MostrarCidadesAgrupadasPrimeiraLetra(string[] cidadesEhEstados)
+        {
             char[] alfabeto = new char[]
-            {
+        {
                  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-            };
+        };
 
-            for (int i = 0;i < alfabeto.Length; i++)
+            for (int i = 0; i < alfabeto.Length; i++)
             {
                 Console.WriteLine($"\nCidades com a letra {alfabeto[i]}\n");
-                for(int x = 0; x < cidadesEhEstados.Length; x++) 
+                for (int x = 0; x < cidadesEhEstados.Length; x++)
                 {
                     char primeiraLetra = alfabeto[i];
                     if (cidadesEhEstados[x] != null && cidadesEhEstados[x].StartsWith(primeiraLetra))
                     {
                         int posicaoInicioEstado = cidadesEhEstados[x].IndexOf(";"); // mostra as cidades sem estado
                         string cidadeSemEstado = cidadesEhEstados[x].Remove(posicaoInicioEstado);
-                        Console.WriteLine("\t" + cidadesEhEstados[x]);
+                        Console.WriteLine("\t" + cidadeSemEstado);
                     }
                 }
             }
+        }
+        static void Main(string[] args)
+        {
+            
+            string caminhoDoArquivo = File.ReadAllText(@"C:\Users\Charles Junior\source\repos\ListaExerciciosString\Exercicio06\Dados\Cidades.csv");
+            string[] cidadesEhEstados = ObterCidadesEhEstados(caminhoDoArquivo);
+
+            MostrarCidadesAgrupadasPrimeiraLetra(cidadesEhEstados);
             
             string[] estados = new string[]
             {
@@ -65,7 +73,7 @@ namespace Exercicio06 //Exercicio do arquivo com as cidades.
                     {
                         int posicaoInicioEstado = cidadesEhEstados[x].IndexOf(";");
                         string cidadeSemEstado = cidadesEhEstados[x].Remove(posicaoInicioEstado);
-                        Console.WriteLine("\t" + cidadesEhEstados[x]);
+                        Console.WriteLine("\t" + cidadeSemEstado);
                     }
                 }
             }
